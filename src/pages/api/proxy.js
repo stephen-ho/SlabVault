3f88245f415c39dcb8eb30557dc73ab94c07ca63
Stephen Ho <wickedcz4a@gmail.com>
2023-03-26 12:17:14 -0700

    API fetches from selected company URL (#8)

--- src/pages/api/proxy.js
+++ src/pages/api/proxy.js
@@ -3,6 +3,15 @@ const axios = require('axios');
 export default function handler(req, res) {
   const company = req.query.company;
   const certNum = req.query.certNum;
+  let companyURL = ``;
+
+  if (company === "PSA") {
+    companyURL = `https://www.psacard.com/cert/${certNum}`;
+  } else if (company === "CGC") {
+    companyURL = `https://www.cgccards.com/certlookup/${certNum}`;
+  } else {
+    companyURL = `https://www.beckett.com/grading/card-lookup?item_type=BGS&item_id=${certNum}`;
+  };
 
   axios({
     method: 'get',
@@ -10,7 +19,7 @@ export default function handler(req, res) {
       "Content-Type": "text/html",
       "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36",
     },
-    url: `https://www.psacard.com/cert/${certNum}`,
+    url: companyURL,
   })
   .then((response) => {
     res.status(200).json(response.data)
